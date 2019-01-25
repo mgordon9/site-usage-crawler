65cb46f33e6db0843c181de010ab51e8b1230031
Matthew Gordon <matthew.gordon@procore.com>
2019-01-25 00:36:44 -0800

    strored top 5 countries for domain

--- app/workers/publisher_crawler.rb
+++ app/workers/publisher_crawler.rb
@@ -10,24 +10,57 @@ class PublisherCrawler
     # TODO: handle unhandled errors
     # TODO: parse out 'http://' and 'https://'
     @domain = domain
+
     # TODO: Get usage for domain
     logger.info("Crawling domain for usage: #{domain}")
+    url = URI.join(DOMAIN_USAGE_URL, domain)
+    page = retrieve_html(url.to_s)
+    rows = parse_top_usage_country_entries(page)
+    country_percentages = {}
+    rows.each do |row|
+      country = parse_country(row)
+      country_percentages[country] = parse_percentage(row)
+    end
+
+    update_domain_countries(country_percentages)
 
     update_domain_link_counts
   end
 
   private
 
+  def parse_top_usage_country_entries(page)
+    page.at_css('table#demographics_div_country_table').css('tbody').css('tr')
+  end
+
+  def parse_country(row)
+    row.css('td')[0].css('a').first.text.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
+  end
+
+  def parse_percentage(row)
+    row.css('td')[1].css('span').first.text.to_f
+  end
+
+  def update_domain_countries(country_percentages)
+    ActiveRecord::Base.transaction do
+      DomainCountry.where(domain: domain).destroy_all
+      country_percentages.each do |country, percentage|
+        DomainCountry.create!(domain: domain, country: country, percentage: percentage)
+      end
+    end
+  end
+
   def update_domain_link_counts
     logger.info("Crawling domain for internal and external links: #{domain}")
-    page = retrieve_html
+    page = retrieve_html(domain)
     link_tags = extract_link_tags(page)
     external_links, internal_links = seperate_external_and_internal_tags(link_tags)
     update_website(external_links, internal_links)
   end
 
-  def retrieve_html
-    response = RestClient.get(domain)
+  def retrieve_html(url)
+    # TODO: handle redirects
+    response = RestClient.get(url)
     Nokogiri::HTML(response)
   end
 
--- spec/workers/publisher_crawler_spec.rb
+++ spec/workers/publisher_crawler_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe PublisherCrawler do
   let(:domain) {"test.com"}
-  let(:example_html) {
+  let(:domain_usage_url) {"https://www.alexa.com/siteinfo/#{domain}"}
+  let(:expected_countries) {
+    [
+      "United States",
+      "United Kingdom",
+      "Canada",
+      "Germany",
+      "France"
+    ]
+  }
+  let(:example_domain_html) {
     <<-HTML
       <!DOCTYPE html>
       <html lang="en" dir="ltr">
-      <head>
+        <head></head>
         <body>
           <div>
             <a href="https://external1.com">hello</a>
@@ -24,16 +34,63 @@ RSpec.describe PublisherCrawler do
       </html>
     HTML
   }
+  let(:example_usage_of_domain_html) {
+    <<-HTML
+    <!DOCTYPE html>
+      <html lang="en" dir="ltr">
+      <head></head>
+        <body>
+          <table cellpadding="0" cellspacing="0" id="demographics_div_country_table" class="table  ">
+            <thead>
+              <tr>
+                <th  style="width: 100px;" class="text-left header">Country</th>
+                <th  style="" class="text-right header">Percent of Visitors</th>
+                <th  style="" class="text-right header">Rank in Country</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr data-count="1" class=" ">
+              <td class=''><a href='/topsites/countries/US'><img class='dynamic-icon' src='/images/flags/us.png' alt='United States Flag'/> &nbsp;United States</a></td>                            <td class='text-right' ><span class=''>41.1%</span></td>                            <td class='text-right' ><span class=''>75</span></td>                    </tr>
+              <tr data-count="2" class=" ">
+              <td class=''><a href='/topsites/countries/GB'><img class='dynamic-icon' src='/images/flags/gb.png' alt='United Kingdom Flag'/> &nbsp;United Kingdom</a></td>                            <td class='text-right' ><span class=''>6.9%</span></td>                            <td class='text-right' ><span class=''>90</span></td>                    </tr>
+              <tr data-count="3" class=" ">
+              <td class=''><a href='/topsites/countries/CA'><img class='dynamic-icon' src='/images/flags/ca.png' alt='Canada Flag'/> &nbsp;Canada</a></td>                            <td class='text-right' ><span class=''>5.4%</span></td>                            <td class='text-right' ><span class=''>38</span></td>                    </tr>
+              <tr data-count="4" class=" ">
+              <td class=''><a href='/topsites/countries/DE'><img class='dynamic-icon' src='/images/flags/de.png' alt='Germany Flag'/> &nbsp;Germany</a></td>                            <td class='text-right' ><span class=''>5.0%</span></td>                            <td class='text-right' ><span class=''>122</span></td>                    </tr>
+              <tr data-count="5" class=" ">
+              <td class=''><a href='/topsites/countries/FR'><img class='dynamic-icon' src='/images/flags/fr.png' alt='France Flag'/> &nbsp;France</a></td>                            <td class='text-right' ><span class=''>3.7%</span></td>                            <td class='text-right' ><span class=''>204</span></td>                    </tr>
+            </tbody>
+          </table>
+        </body>
+      </html>
+    HTML
+  }
 
-  context 'wesbite entry for the given domain exists' do
-    it 'updates internal and external links' do
+  def create_domain_countries(domain)
+    DomainCountry.create!(domain: domain, country: 'country1', percentage: 20.1)
+    DomainCountry.create!(domain: domain, country: 'country2', percentage: 1.1)
+    DomainCountry.create!(domain: domain, country: 'country3', percentage: 3.2)
+    DomainCountry.create!(domain: domain, country: 'country4', percentage: 9.6)
+    DomainCountry.create!(domain: domain, country: 'country5', percentage: 32.4)
+  end
+
+  context 'wesbite entry and domain countries entries for the given domain exist' do
+    it 'updates internal and external links and changes the domain countries entries' do
+      create_domain_countries(domain)
+      create_domain_countries('domain2')
       Website.create!(domain: domain, num_external_links: 5, num_internal_links: 6)
       Website.create!(domain: 'domain2', num_external_links: 1, num_internal_links: 0)
 
+      WebMock.stub_request(:get, domain_usage_url).
+        and_return(status: 200, body: example_usage_of_domain_html)
       WebMock.stub_request(:get, "http://#{domain}/").
-        and_return(status: 200, body: example_html)
+        and_return(status: 200, body: example_domain_html)
 
       subject.perform(domain)
+
+      countries = DomainCountry.where(domain: domain).pluck(:country)
+      expect(countries).to match_array(expected_countries)
+
       website = Website.find_by(domain: domain)
       expect(website.num_external_links).to eq(3)
       expect(website.num_internal_links).to eq(2)
@@ -42,12 +99,19 @@ RSpec.describe PublisherCrawler do
 
   context 'wesbite entry for the given domain DOES NOT exist' do
     it 'creates a new wesbite entry' do
+      create_domain_countries('domain2')
       Website.create!(domain: 'domain2', num_external_links: 1, num_internal_links: 0)
 
+      WebMock.stub_request(:get, domain_usage_url).
+        and_return(status: 200, body: example_usage_of_domain_html)
       WebMock.stub_request(:get, "http://#{domain}/").
-        and_return(status: 200, body: example_html)
+        and_return(status: 200, body: example_domain_html)
 
       expect{subject.perform(domain)}.to change{Website.count}.by(1)
+
+      countries = DomainCountry.where(domain: domain).pluck(:country)
+      expect(countries).to match_array(expected_countries)
+
       website = Website.find_by(domain: domain)
       expect(website.num_external_links).to eq(3)
       expect(website.num_internal_links).to eq(2)
